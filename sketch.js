6473b4ebd052002f01c619ce61d542978c71b902
Mahi0308 <72852795+Mahi0308@users.noreply.github.com>
2020-10-19 23:35:23 +1400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,120 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground,stand1,stand2,slingShot;
+
+var gameState  ="onSling"
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+
+
+
+    ground = new Ground();
+    stand1 = new Stand(390, 300, 250, 10);
+    stand2 = new Stand(700,200,200,10);
+
+
+    box1 = new Box (300,275,30,40);
+    box2 = new Box (330,275,30,40);
+    box3 = new Box (360,275,30,40);
+    box4 = new Box (390,275,30,40);
+    box5 = new Box (420,275,30,40);
+    box6 = new Box (450,275,30,40);
+    box7 = new Box (480,275,30,40);
+    box8 = new Box (330,235,30,40);
+    box9 = new Box (360,235,30,40);
+    box10 = new Box (390,235,30,40);
+    box11 = new Box (420,235,30,40);
+    box12 = new Box (450,235,30,40);
+    box13 = new Box (360,195,30,40);
+    box14 = new Box (390,195,30,40);
+    box15 = new Box (420,195,30,40);
+    box16 = new Box (390,155,30,40);
+   block1 = new Box (640,175,30,40);
+   block2 = new Box (670,175,30,40);
+   block3 = new Box (700,175,30,40);
+   block4 = new Box (730,175,30,40);
+  block5 = new Box (760,175,30,40);
+  block6 = new Box (670,135,30,40);
+  block7 = new Box (700,135,30,40);
+  block8 = new Box (730,135,30,40);
+  block9 = new Box (700,95,30,40);
+  ball = Bodies.circle (50,200,20);
+  World.add(world,ball);
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(this.ball,{x:200, y:50});
+}
+
+function draw(){
+    background(56,44,44);
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    ground.display();
+    stand1.display();
+    stand2.display();
+    strokeWeight(2);
+    stroke(15);
+    fill ("skyblue")
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    fill ("pink")
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    fill ("turquoise")
+    box13.display();
+    box14.display();
+    box15.display();
+    fill ("grey")
+    box16.display();
+    fill ("skyblue")
+   block1.display();
+   block2.display();
+   block3.display();
+   block4.display();
+   block5.display();
+   block6.display();
+   block7.display();
+   block8.display();
+   block9.display();
+
+  
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingShot.attach(this.ball)
+    }
+  }
